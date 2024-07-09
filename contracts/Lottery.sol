33ce5159620003922745dbdb2ee6ca6661e35083
Hrittik20 <hrittikrc20@gmail.com>
2024-07-09 14:40:14 +0530

    First commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .001 ether);
+
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);  // transfer all the money in the contract to the winner
+        players = new address[](0); // reset the players array
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+}
